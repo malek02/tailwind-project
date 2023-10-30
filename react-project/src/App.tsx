efcf8a1bb59f51801b5b5ced3a86bf89b0c3a98d
Malek Mechi <malek.mechi.ext@aleia.com>
2023-10-30 12:20:39 +0100

    feat(tailwindcss): start project

---
+++ react-project/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <div className=" h-full text-red-500 justify-center flex flex-col items-center ">
+        <h1>Hello tailwind</h1>
+        <h3>im make in some new css here</h3>
+      </div>
+    </>
+  );
+}
+
+export default App;

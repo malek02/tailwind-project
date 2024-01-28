82ba5a26497fb584bc30024e22d5cfa66905a0e7
Malek Mechi <malek.mechi.ext@aleia.com>
2024-01-28 15:31:15 +0100

    add-eslint-rules

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -1,6 +1,17 @@
 import React from "react";
 
 function App() {
+
+   React.useEffect (()=>{
+    const eventSource = new EventSource("http://localhost:8082/stream");
+    
+    // attaching a handler to receive message events
+    eventSource.onmessage = (event) => {
+      const stockData = JSON.parse(event.data);
+      console.log(stockData)
+    };
+   },[])
+
   return (
     <>
       <div className=" h-full text-red-500 justify-center flex flex-col items-center ">
